13fef730b9a210dee4ae53f462539491d47102c2
sai charan <99958378+charanchitti17@users.noreply.github.com>
2023-07-24 16:16:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+panels = document.querySelectorAll(".panel")
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActive();
+        panel.classList.add("active");
+    })
+})
+const removeActive = () => {
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    })
+}
